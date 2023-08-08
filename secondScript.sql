e894f46e254a0791b478a697a8b635b36ecf345c
Evgeniy Putilin <mr.putilinz@gmail.com>
2023-08-08 19:53:48 +0300

    add files from 2nd HW

--- secondScript.sql
+++
@@ -1,2 +0,0 @@
-select name, surname, city_of_living from first_homework.persons
-where city_of_living = 'Moscow';
--- thirdScript.sql
+++
@@ -1,5 +0,0 @@
-select * from first_homework.persons
-where age > 27
-order by age desc;
-
-
